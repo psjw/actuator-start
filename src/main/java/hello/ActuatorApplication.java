327f49e81a5602609f9280252fa173166d5c15ca
psjw <dlrudtn108@gmail.com>
2025-04-28 11:48:14 +0900

    gague

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,5 +1,7 @@
 package hello;
 
+import hello.order.gague.StockConfigV1;
+import hello.order.gague.StockConfigV2;
 import hello.order.v0.OrderConfigV0;
 import hello.order.v1.OrderConfigV1;
 import hello.order.v2.OrderConfigV2;
@@ -19,7 +21,9 @@ import org.springframework.context.annotation.Import;
 //@Import(OrderConfigV1.class)
 //@Import(OrderConfigV2.class)
 //@Import(OrderConfigV3.class)
-@Import(OrderConfigV4.class)
+//@Import(OrderConfigV4.class)
+//@Import({OrderConfigV4.class, StockConfigV1.class})
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = {"hello.controller"})
 public class ActuatorApplication {
     @Autowired
