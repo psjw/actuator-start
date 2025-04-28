4edd7544b573d09cf6d1eea2da20560a89615b61
psjw <dlrudtn108@gmail.com>
2025-04-28 10:30:52 +0900

    v3

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,18 +1,24 @@
 package hello;
 
-import hello.order.v0.OrderConfigV0;
 import hello.order.v1.OrderConfigV1;
+import hello.order.v2.OrderConfigV2;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
+@Slf4j
 //@Import(OrderConfigV0.class)
-@Import(OrderConfigV1.class)
-@SpringBootApplication(scanBasePackages = "hello.controller")
+//@Import(OrderConfigV1.class)
+@Import(OrderConfigV2.class)
+@SpringBootApplication(scanBasePackages = {"hello.controller"})
 public class ActuatorApplication {
+    @Autowired
+    MeterRegistry meterRegistry;
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
